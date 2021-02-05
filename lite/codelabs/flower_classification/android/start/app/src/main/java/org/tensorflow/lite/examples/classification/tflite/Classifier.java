5e1ef96c4483ee154cd2e80e74306eb5f7fe4027
leowu <wuliang0306@gmail.com>
2021-02-05 08:32:10 -0800

    Complete the app

--- lite/codelabs/flower_classification/android/start/app/src/main/java/org/tensorflow/lite/examples/classification/tflite/Classifier.java
+++ lite/codelabs/flower_classification/android/start/app/src/main/java/org/tensorflow/lite/examples/classification/tflite/Classifier.java
@@ -72,7 +72,7 @@ public abstract class Classifier {
 
   /** An instance of the driver class to run model inference with Tensorflow Lite. */
   // TODO: Declare a TFLite interpreter
-
+  protected Interpreter tflite;
 
   /** Options for configuring the Interpreter. */
   private final Interpreter.Options tfliteOptions = new Interpreter.Options();
@@ -176,6 +176,7 @@ public abstract class Classifier {
   /** Initializes a {@code Classifier}. */
   protected Classifier(Activity activity, Device device, int numThreads) throws IOException {
     tfliteModel = FileUtil.loadMappedFile(activity, getModelPath());
+    tflite = new Interpreter(tfliteModel, tfliteOptions);
     switch (device) {
       case GPU:
         // TODO: Create a GPU delegate instance and add it to the interpreter options
@@ -231,6 +232,7 @@ public abstract class Classifier {
     Trace.beginSection("runInference");
     long startTimeForReference = SystemClock.uptimeMillis();
     // TODO: Run TFLite inference
+    tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
 
     long endTimeForReference = SystemClock.uptimeMillis();
     Trace.endSection();
@@ -239,6 +241,7 @@ public abstract class Classifier {
     // Gets the map of label and probability.
     // TODO: Use TensorLabel from TFLite Support Library to associate the probabilities
     //       with category labels
+    Map<String, Float> labeledProbability = new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer)).getMapWithFloatValue();
 
     Trace.endSection();
 
@@ -250,6 +253,8 @@ public abstract class Classifier {
   public void close() {
     if (tflite != null) {
       // TODO: Close the interpreter
+      tflite.close();
+      tflite = null;
 
     }
     // TODO: Close the GPU delegate
@@ -279,10 +284,10 @@ public abstract class Classifier {
     // TODO: Define an ImageProcessor from TFLite Support Library to do preprocessing
     ImageProcessor imageProcessor =
             new ImageProcessor.Builder()
-
-
-
-
+                .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                .add(new ResizeOp(imageSizeX, imageSizeY, ResizeMethod.NEAREST_NEIGHBOR))
+                .add(new Rot90Op(numRoration))
+                .add(getPreprocessNormalizeOp())
                 .build();
     return imageProcessor.process(inputImageBuffer);
   }
